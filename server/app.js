314d38c557a70c240788f073a2a70f59910b37ac
ochowo <ochowoikongbeh@gmail.com>
2018-11-01 23:37:14 +0100

    feat(endpoint): get all order[delivers #161601736]

--- server/app.js
+++ server/app.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import db from './db/db';
 // Set up the express app
 const app = express();
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 // get all orders
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on port ${port}....`));
-export default app;
+app.get('/api/orders', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    message: 'orders retrieved successfully',
+    orders: db,
+  });
+});
+// get a single order
+app.get('/api/orders/:id', (req, res) => {
+  const order = db.find(c => c.id === parseInt((req.params.id), 10));
+  if (!order) res.status(404).send('Order does not exist');
+  res.status(200).send({
+    success: 'true',
+    message: 'order retrieved successfully',
+    order,
+  });
+});
