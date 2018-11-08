03fe66f305404856b05e197f899341ae22ebc943
ochowo <ochowoikongbeh@gmail.com>
2018-11-08 21:28:47 +0100

    chore(database): set up database [finishes ##161754472]

--- server/UsingJSObject/app.js
+++ server/UsingJSObject/app.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import router from './router/index';
+
+// import router from './router/index';
 // Setup the express app
 const app = express();
 // Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/api/v1', router);
+// app.use('/api/v1', router);
 app.get('/', (req, res) => {
   res.send('Visit /api/v1 to view API cheers!!!');
 });
